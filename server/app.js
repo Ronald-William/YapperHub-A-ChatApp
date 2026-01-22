4ba06006faa0a24df8244193fd1907b84b2450d5
Ronald Joseph <ronaldjoseph439@gmail.com>
2026-01-22 20:31:35 +0530

    Implement secure get and fetch methods for messages

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
 import authRoutes from "./routes/authRoutes.js"
+import msgRoutes from "./routes/msgRoutes.js"
 
 const app = express();
 
@@ -19,8 +20,9 @@ app.use(
 );
 
 
+app.use("/api/auth", authRoutes)
 
-app.use("/api/auth", authRoutes);
+app.use("/api/messages", msgRoutes);
 
 
 export default app;
---
+++ server/routes/msgRoutes.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import Message from '../models/Message.js';
+import {protect} from "../middleware/authMiddleware.js"
+const router = express.Router();
+router.post("/", protect, async (req, res) => {
+    const { receiver, text } = req.body;
+
+    const msg = await Message.create({
+        sender: req.user._id,
+        receiver,
+        text
+    });
+
+    res.json(msg);
+});
+
+router.get("/:userId", protect, async(req,res)=>{
+    const messages = await Message.find({
+        $or: [
+            {sender: req.user._id, receiver: req.params.userId},
+            {sender: req.params.userId, receiver: req.user_id}
+        ]
+    }).sort({createdAt: 1});
+    res.json(messages);
+})
+
+export default router;
