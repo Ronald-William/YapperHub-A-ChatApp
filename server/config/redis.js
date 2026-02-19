79a3168f0b02bc687e64264a46274eba83704d2a
Ronald Joseph <ronaldjoseph439@gmail.com>
2026-02-19 22:27:56 +0530

    redis setup and testing

---
+++ server/config/redis.js
@@ -0,0 +1,19 @@
+import Redis from "ioredis";
+
+const redis = new Redis({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    retryStrategy: (times)=>{
+        const delay = Math.min(times*50, 2000);
+        return delay;
+    }
+});
+
+redis.on('connect', () =>{
+    console.log("redis connected");
+})
+redis.on('error',(err)=>{
+    console.log('Redis error: ',err);
+})
+
+export default redis;
